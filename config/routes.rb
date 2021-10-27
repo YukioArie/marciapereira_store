dc5ecaf1add6aa1ffbc6992990a9eadb5fa30330
Yukio Arie <yukioarie@gmail.com>
2021-10-26 21:37:26 -0300

    add controller site/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'home/index'
+  end
+  root to: 'site/home#index'
+  get 'home/index'
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
