cf8720fab1803199fb2f2fe936961addb7398a69
Yukio Arie <yukioarie@gmail.com>
2021-10-28 19:32:19 -0300

    fix field_with_errors html input forms

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# fix field_with_errors htmls imput
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
+end
