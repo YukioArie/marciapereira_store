0ff89a7dee488284f15a5e04ed83dea1c8770663
Yukio Arie <yukioarie@gmail.com>
2021-10-30 02:54:36 -0300

    add bootstrap icons via yarn and logo img store

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//images
+require.context('../images', true)
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -11,8 +13,9 @@ import "channels"
 import "jquery"
 import "bootstrap";
 import "../stylesheets/application.scss"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
-global.toastr = require("toastr")
+
 
 Rails.start()
 Turbolinks.start()
