90c55ad75b97ea0675b844d282891f4446fce112
Yukio Arie <yukioarie@gmail.com>
2021-12-01 14:31:16 -0300

    create page new product

--- app/controllers/admins_area/products_controller.rb
+++ app/controllers/admins_area/products_controller.rb
@@ -1,5 +1,28 @@
-class AdminsArea::ProductsController < ApplicationController
+class AdminsArea::ProductsController < AdminsAreaController
   def index
     @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit; end
+
+  def create
+    @product = Product.new(params_product)
+    if @product.save!
+      redirect_to admins_area_products_path, notice: "Produto
+      cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_product
+    params.require(:product).permit(:title, :price, :description, images: [],
+                                                                  sizes_attributes: %i[id size_type quantity _destroy])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  has_many :sizes, inverse_of: :product
   has_many_attached :images
+  accepts_nested_attributes_for :sizes, reject_if: :all_blank, allow_destroy: true
+  
 end
