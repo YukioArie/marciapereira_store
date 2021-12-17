70306df81ec52dc79bc5f4754cb9dc1d14a0c16e
Yukio Arie <yukioarie@gmail.com>
2021-12-16 23:54:47 -0300

    add in Item model method class create_in_cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :cart
   belongs_to :size
+
+  def self.create_in_cart(client_id, size_id)
+    unless Cart.where(client_id: client_id).first.items.include?(where(size_id: size_id).first)
+      create!(
+        quantity: 1,
+        cart_id: Client.find(client_id).cart.id,
+        size: Size.find(size_id)
+      )
+    end
+  end
 end
