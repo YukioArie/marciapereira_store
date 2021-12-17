2fd9eed0c8246f723a9fee5ed61ca6169246d048
Yukio Arie <yukioarie@gmail.com>
2021-12-17 15:16:21 -0300

    create scope for search product term

--- app/controllers/admins_area/search_controller.rb
+++ app/controllers/admins_area/search_controller.rb
@@ -1,7 +1,7 @@
 class AdminsArea::SearchController < AdminsAreaController
   def products
     console
-    @products = Product.includes(:images_blobs, :sizes).where('lower(title) LIKE ?', "%#{params[:title]}%")
+    @products = Product._search_(params[:title])
     @clientes = Client.all.order(:name)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ApplicationRecord
   has_many :sizes, inverse_of: :product
   has_many_attached :images
   accepts_nested_attributes_for :sizes, reject_if: :all_blank, allow_destroy: true
-  
+  scope :_search_, lambda { |params|
+                     includes(:images_blobs, :sizes)
+                       .where('lower(title) LIKE ?', "%#{params}%")
+                   }
 end
