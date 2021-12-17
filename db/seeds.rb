e63deaea1fdd04d1462dada9fa719ee22d399f09
Yukio Arie <yukioarie@gmail.com>
2021-12-16 23:57:01 -0300

    add method generate clients and carts in seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+genre = %w[masculino feminino]
+
+50.times do |index|
+  client = Client.new(
+    id: index,
+    name: Faker::Name.name,
+    genre: genre.sample,
+    email: Faker::Internet.email,
+    cellphone_number: Faker::Number.number(digits: 4)
+  )
+  cart = Cart.new(
+    id: index,
+    total_price: 0,
+    client_id: client.id
+  )
+  client.save!
+  cart.save!
+end
