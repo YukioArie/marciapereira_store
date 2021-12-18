5d501072e15e0737f74025f327bc9d12c46e0171
Yukio Arie <yukioarie@gmail.com>
2021-12-18 01:21:45 -0300

    add kaminari theme bootstrap

--- app/controllers/admins_area/search_controller.rb
+++ app/controllers/admins_area/search_controller.rb
@@ -1,7 +1,7 @@
 class AdminsArea::SearchController < AdminsAreaController
   def products
     console
-    @products = Product._search_(params[:title])
+    @products = Product._search_(params[:title], params[:page])
     @clients = Client.all.order(:name)
   end
 end
