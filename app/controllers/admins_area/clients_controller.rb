1734118944f2f597e03028b51f863a6fefb9abb6
Yukio Arie <yukioarie@gmail.com>
2022-01-05 13:09:00 -0300

    add action destroy on clients_controller

--- app/controllers/admins_area/clients_controller.rb
+++ app/controllers/admins_area/clients_controller.rb
@@ -7,11 +7,21 @@ class AdminsArea::ClientsController < AdminsAreaController
     @client = Client.new
   end
 
+  def destroy
+    @client = Client.find(params[:id])
+    @cart = Cart.where(client_id: params[:id]).first
+    if @cart.destroy! && @client.destroy!
+      redirect_to admins_area_clients_path, notice: 'Cliente excluido com sucesso!'
+    else
+      render :index
+    end
+  end
+
   def create
     @client = Client.new(params_client)
     if @client.save!
       Cart.create!(total_price: 0, client_id: @client.id)
-      redirect_to admins_area_clients_path, notice: 'Produto cadastrado com sucesso!'
+      redirect_to admins_area_clients_path, notice: 'Cliente cadastrado com sucesso!'
     else
       render :new
     end
