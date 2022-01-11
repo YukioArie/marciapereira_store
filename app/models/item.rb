0a437c3641d64851f8c944ed3dff8850f6275cf9
Yukio Arie <yukioarie@gmail.com>
2022-01-10 22:38:25 -0300

    fix bug duplicate sizes items in cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :size
 
   def self.create_in_cart(client_id, size_id)
-    unless Cart.where(client_id: client_id).first.items.include?(where(size_id: size_id).first)
+    unless Cart.where(client_id: client_id).first.items.pluck(:size_id).include?(size_id.to_i)
       create!(
         quantity: 1,
         cart_id: Client.find(client_id).cart.id,
