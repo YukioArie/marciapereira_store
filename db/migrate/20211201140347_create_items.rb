2f0bd375653337c4de2d3ad78bdf08a65711dedc
Yukio Arie <yukioarie@gmail.com>
2022-01-14 20:09:56 -0300

    permit  cart_id from items receive null

--- db/migrate/20211201140347_create_items.rb
+++ db/migrate/20211201140347_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.integer :quantity
-      t.references :cart, null: false, foreign_key: true
+      t.references :cart, null: true, foreign_key: true
       t.references :size, null: false, foreign_key: true
 
       t.timestamps
